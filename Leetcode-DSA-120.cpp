5b7daa9fa8d54a26dd8d8adb1fe2a17a22b19afb
akari <pikachungaytho@gmail.com>
2025-09-25 14:08:08 +0700

    finished

--- Leetcode-DSA-120.cpp
+++ Leetcode-DSA-120.cpp
@@ -10,13 +10,17 @@ public:
         int ans = INT_MAX;
         dp[0][0] = triangle[0][0];
         for (int i = 1; i < n; ++i){
-            for (int j = 0; j <= i; +j){
-                dp[i][j] = triangle[i][j] + min(dp[i-1][j-1], dp[i-1][j]);
-                if (i == n - 1){
-                    ans = max(ans, dp[i][j]);
-                }
+            dp[i][0] = triangle[i][0] + dp[i-1][0];
+            for (int j = 1; j < i; ++j){
+                dp[i][j] = triangle[i][j] + min(dp[i-1][j], dp[i-1][j-1]);
             }
+            dp[i][i] = triangle[i][i] + dp[i-1][i-1];
         }
+
+        for (int i = 0; i < n; ++i){
+            ans = min(ans, dp[n-1][i]);
+        }
+
         return ans;
     }
 };
